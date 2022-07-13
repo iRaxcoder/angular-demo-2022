5561e07728899ce72bdc5ab924ae063c8f45e251
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-07-12 18:37:25 -0600

    feat: add app.component unit testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -6,7 +7,8 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatMenuModule
       ],
       declarations: [
         AppComponent
@@ -26,10 +28,24 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-demo');
   });
 
-  it('should render title', () => {
+  it('should render button Inicio', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-demo app is running!');
+    expect(compiled.querySelector('.btn-inicio')?.textContent).toContain('Inicio');
   });
+
+  it('should render button Inicio', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.btn-inicio')?.textContent).toContain('Inicio');
+  });
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.menu h1')?.textContent).toContain('angular-demo is running!');
+  // });
 });
